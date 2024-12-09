70e1c66ad25fdb07c038ad0883ddebdc74ccb9f0
Somenath Choudhury <somenathchoudhury35@gmail.com>
2024-12-09 14:49:17 +0530

    commited

--- src/middleware.js
+++ src/middleware.js
@@ -1,18 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+const isPublicRoute = createRouteMatcher(['/']);
 
 
-export default clerkMiddleware(async (auth, request) => {
-  if (isProtectedRoute(request)) {
-    await auth.protect();
-  }
+export default clerkMiddleware((auth, req) => {
+
+  if (!isPublicRoute(req)) auth().protect();
+
 });
 
+
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
